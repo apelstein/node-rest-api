2ad9810a840ffae303fcbccad21076b56de81ed3
Gilad Apelstein <34208990+apelstein@users.noreply.github.com>
2020-03-29 23:18:18 +0300

    adding file filter support and limit the file size

--- sampsite/routes/products.js
+++ sampsite/routes/products.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 const mongoose = require('mongoose');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb){
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10000000
+  },
+  fileFilter: fileFilter
+});
 const Product = require('../models/product');
 
 /* GET home page. */
