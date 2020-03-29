84761884245a1e128fb1799de7c20c7f32e19271
Gilad Apelstein <34208990+apelstein@users.noreply.github.com>
2020-03-29 13:26:37 +0300

    implement GET orders

--- sampsite/routes/orders.js
+++ sampsite/routes/orders.js
@@ -7,8 +7,26 @@ const Order = require('../models/order');
 
 /* GET orders listing. */
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-      message: 'Orders were fetched'
+  Order.find().select('_id product quantity').exec().then(docs => {
+      const response = {
+          count: docs.length,
+          orders: docs.map(doc => {
+              return {
+                  _id: doc._id,
+                  product: doc.product,
+                  quantity: doc.quantity,
+
+                  request: {
+                      type: "GET",
+                      url: "http://localhost:3000/orders/" + doc._id
+                  }
+              }
+          })
+      }
+      res.status(200).json(response);
+  }).catch(err => {
+      console.log(err);
+      res.status(500).json({error: err});
   });
 });
 
